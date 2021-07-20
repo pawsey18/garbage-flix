f4c8f8c10eb3089fb2798f9929d6f5d45429cd95
Nick Pawsey <pawsey18@gmail.com>
2021-07-20 19:40:56 -0300

    demo

--- garbage-flix/src/app/app.component.ts
+++ garbage-flix/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'garbage-flix';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 1400.0,
+        },
+
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 1400.0,
+        },
+      ];
+    }, 1000);
+  }
+
+  title = 'fuck you';
+  movies;
+
+  multiplyNumber(n: number) {
+    return n * 2;
+  }
 }
